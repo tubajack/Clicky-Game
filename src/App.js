17ab82cc24bc6ac9e09a627d324f071fcac526f4
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-10 18:29:17 -0500

    Set up the basic framework for the render function in the react app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //Import the react.JS files that are needed
 import react, {Component} from "react"
 import reactDOM from "react-dom";
+import coaches from "./images"
 
 //Initialize the beginning of the game
 class App extends Component(){
@@ -9,11 +10,29 @@ class App extends Component(){
         selectedCoaches = [], 
         points = 0
     };
-}
+
 
 //The coach is removed from the array once it is clicked
 
 
+
 //Click on a previously selected coach, the game is reset. YOU LOSE. 
 
+
+//Click on a fish that has not previously been selected, the score is increased. 
+
+
+//If your score hits 12, YOU WIN. The game is reset. 
+
+
+//Make sure you render all components
+render(){
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+}
 export default App;
