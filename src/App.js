0b2764beb677112c68b171a528f031483e295fa6
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-10 23:14:21 -0500

    Am working on the function which removes the coach from the array once clicked.

--- src/App.js
+++ src/App.js
@@ -13,13 +13,20 @@ class App extends Component(){
 
 
 //The coach is removed from the array once it is clicked
+pictureClick = event => {
+    //Figure out which coach was clicked last. 
+    const currentCoach = event.target.alt;
 
+    //Remove that coach from the array. 
+    const chosenCoach = this.state.selectedCoaches.indexof(selectedCoaches);
+
+}
 
 
 //Click on a previously selected coach, the game is reset. YOU LOSE. 
 
 
-//Click on a fish that has not previously been selected, the score is increased. 
+//Click on a coach that has not previously been selected, the score is increased. 
 
 
 //If your score hits 12, YOU WIN. The game is reset. 
