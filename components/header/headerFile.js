113eac0f53cbf5f2197d6f53cb726c27bff9bd35
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-11 19:05:21 -0500

    Finishing off the functionality of the header file. Am going to need to test this somehow.

--- components/header/headerFile.js
+++ components/header/headerFile.js
@@ -4,7 +4,11 @@ class Header extends Component{
     render(){
         return(
             <div class= "navbar navbar-default navbar-fixed-top">
-                
+                <ul>
+                    <li className = "item-left">These cards feature 12 of the best coaches in women's college basketball.</li>
+                    <li className = "item-center"></li>
+                    <li className = "item-right">Current Score: {this.props.points}</li>
+                </ul>
             </div>
 
         );
