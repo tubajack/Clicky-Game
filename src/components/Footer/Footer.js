acb277ff6abf12fe37deb907d15d2ff4254d40ee
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-11 22:54:02 -0500

    This game was challenging to complete. Thankfully, the footer isnt too terribly difficult.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//Dont forget to capitalize React at the beginning
+import React from "react"
+
+const Footer = () => (
+    <header className = "footer">
+        <p>Creating this game was challenging. The Scrimba tutorials are helpful for learning React.</p>
+    </header>
+
+);
+
+export default Footer;
