248bdf98e54a6cdd9e8c603f95a000f6d7ddb25f
Jackson Henry <jackson.henry13@yahoo.com>
2019-07-20 18:34:50 -0500

    Got some help from the TAs to finish this game. Here are some of the changes that I have made to the game.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 //Import the react.JS files that are needed
 import React, {Component} from "react"
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom"
 import Header from "./components/header"
-import coachCards from "./components/coachCards"
+import CoachCards from "./components/coachCards"
 import Footer from "./components/Footer"
-import Coaches from "./images"
-
+//import Coaches from "./src/images"
+import image1 from "./images/BrendaFrese.jpg"
+import image2 from "./images/DawnStaley.jpg"
 //Initialize the beginning of the game
-class App extends Component(){
+class App extends Component{
     state = {
-        Coaches, 
+        Coaches: [], 
         selectedCoaches: [], 
-        points: 0
+        score: 0
     };
+    componentDidMount(){
+        this.setState({
+           Coaches: [{id: 1, images: image1, clicked: false}, {id: 2, images: image2, clicked: false}]
+        })
+    }
 
     //The coach is removed from the array once it is clicked
-    pictureClick = event => {
+    imageClick = event => {
         //Figure out which coach was clicked last. 
-        const currentCoach = event.target.alt;
+        console.log(event.target)
+        this.setState({
+            currentCoach: event.target.alt
+        })
 
-        //Remove that coach from the array. 
-        const chosenCoach = this.state.selectedCoaches.indexof(currentCoach) > -1;
+        // //Remove that coach from the array. 
+        // const chosenCoach = this.state.selectedCoaches.indexof(currentCoach) > -1;
 
     }
 
@@ -41,33 +50,33 @@ class App extends Component(){
     //For some reason, I get an error with my else statement when I dont have a set of parenthesis around it. This is very strange
     //Click on a coach that has not previously been selected, the score is increased. 
     else(){
-        this.setState({
-                //Sort the coaches in a random order
-                coaches: this.state.coaches.sort(function(a,b){
-                    return Math.random() - 0.5
-                }),
-                //Add the selected coach to the array of coaches that have been selected
-                selectedCoaches: this.state.selectedCoaches.concat(currentCoach),
-                //Don't forget to update the score
-                score: this.state.score + 1
-            },
+        // this.setState({
+        //         //Sort the coaches in a random order
+        //         coaches: this.state.coaches.sort(function(a,b){
+        //             return Math.random() - 0.5
+        //         }),
+        //         //Add the selected coach to the array of coaches that have been selected
+        //         selectedCoaches: this.state.selectedCoaches.concat(currentCoach),
+        //         //Don't forget to update the score
+        //         score: this.state.score + 1
+        //     },
 
-            //What happens when someone gets all 12 of the coaches correctly?
-            () => {
-                if(this.state.points === 12){
-                    alert("Congratulations! You have won the game.");
-                    //Once again, you have to shuffle the cards in a random order
-                    this.setState({
-                        coaches: this.state.coaches.sort(function(a,b){
-                            return Math.random() - 0.5;
-                        }), 
-                        selectedCoaches: [],
-                        score: 0
-                    });
+        //     //What happens when someone gets all 12 of the coaches correctly?
+        //     () => {
+        //         if(this.state.points === 12){
+        //             alert("Congratulations! You have won the game.");
+        //             //Once again, you have to shuffle the cards in a random order
+        //             this.setState({
+        //                 coaches: this.state.coaches.sort(function(a,b){
+        //                     return Math.random() - 0.5;
+        //                 }), 
+        //                 selectedCoaches: [],
+        //                 score: 0
+        //             });
                     
-                }
-            }  
-        );
+        //         }
+        //     }  
+        // );
     }
     
     //At the end of the day, make sure to render all components
@@ -75,15 +84,8 @@ class App extends Component(){
         return(
             <div>
                 <Header score = {this.state.score} />
-                <div class name = "wrapper">
-                    {this.state.Coaches.map(Coaches => (
-                        <coachCards imageClick = {this.imageClick} 
-                        id = {Coaches.id}
-                        key = {Coaches.id}
-                        image = {Coaches.image}
-                        />
-                    ))}
-                </div>
+                <CoachCards/>
+    
                 <Footer />
             </div>
             
